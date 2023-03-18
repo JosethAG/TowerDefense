b7c86eef5e4c4b491f72aecf9b32bffcc64f34b9
ValeryACS <valeryandrea2914@gmail.com>
2023-03-18 00:08:25 -0600

    fix: correción de clase lista

--- ProyectoTowerDefense/src/proyectotowerdefense/Lista.java
+++ ProyectoTowerDefense/src/proyectotowerdefense/Lista.java
@@ -9,5 +9,85 @@ package proyectotowerdefense;
  * @author Usuario
  */
 public class Lista {
+    private Nodo cabeza;
     
+    public void inserta(Persona p){
+        if(cabeza==null){
+            cabeza = new Nodo(p);
+        }else if (p.getId()<cabeza.getDato.getId()){
+            Nodo aux = new Nodo(p);
+            aux.setNext(cabeza);
+            cabeza = aux;
+        }else if(cabeza.getNext()==null){
+            cabeza.setNext(new Nodo(p));
+        }else{
+            Nodo aux = cabeza;
+            while(aux.getNext() != null && aux.getNext().getDato().getId() < p.getId()){
+                aux=aux.getNext();
+        }
+            Nodo temp = new Nodo(p);
+            temp.setNext(aux.getNext());
+            aux.setNext(temp);
+        }
+    }
+    public boolean existe(int id){
+        boolean esta = false;
+        if(cabeza != null){
+            Nodo aux = cabeza;
+            
+            while(aux != null && aux.getDato().getId() < id){
+                aux = aux.getNext();
+            }
+            esta = (aux != null && aux:getDato().getId()==id);
+        }
+        return esta;
+    }
+    public void modifica(persona p){
+        if(cabeza != null){
+            Nodo aux = cabeza;
+            while(aux != null && aux.getDato().getId() < p.getId()){
+            aux = aux.getNext();
+        }
+            if(aux != null && aux.getDato().getId()==p.getId()){
+                aux.getDato().setNombre(p.getNombre());
+            }
+        }
+    }
+    public void elimina(int id){
+        if(cabeza != null){
+            if(cabeza.getDato().getId()==id)
+            {
+                cabeza = cabeza.getNext();
+            }
+            else{
+                Nodo aux = cabeza;
+                while(aux.getNext() != null && 
+                        aux.getNext().getDato().getId() < id){
+                    aux = aux.getNext();
+                }
+                if(aux.getNext() != null &&
+                        aux.getNext().getDato().getId() < id){
+                    aux.setNext(aux.getNext().getNext());
+                }
+            }
+        }
+    }
+    public Persona extrae(int id){
+        Persona p = null;
+        if(cabeza != null){
+            if(cabeza.getDato().getId()==id){
+                cabeza = cabeza.getNext();
+            }else{
+                Nodo aux = cabeza;
+                while(aux.getNext() != null && aux.getNext().getDato().getId() < id){
+                    aux = aux.getNext();
+                }
+                if (aux.getNext() != null && aux.getNext().getDato().getId()==id){
+                    p = aux.getNext().getDato();
+                    aux.setNext(aux.getNext().getNext());
+                }
+            }
+        }
+        return p;
+    }
 }

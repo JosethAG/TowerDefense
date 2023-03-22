bde6cd32f280ac219bef9a1686d6bbfb69326868
ADelgado-99 <angelodr44@gmail.com>
2023-03-22 15:06:57 -0600

    feat: Validacion Interaccion Tropas en el Main
    Se agrega en el main la validacion para las interacciones de las tropas

--- ProyectoTowerDefense/src/proyectotowerdefense/Main.java
+++ ProyectoTowerDefense/src/proyectotowerdefense/Main.java
@@ -17,9 +17,32 @@ public class Main {
         // TODO code application logic here
         Pila Camino1cpu = new Pila();
         Pila Camino1player = new Pila();
+        Pila Camino2cpu = new Pila();
+        Pila Camino2player = new Pila();
         InteraccionesTropa interaccion = new InteraccionesTropa();
-        interaccion.CombateMago(Camino1cpu, Camino1player);
+
+        //Validacion para interaccion entre Camino1 CPU y Player
+        if (Camino1cpu.getCima().getValor().getTipo() == 1) {
+            interaccion.CombateMago(Camino1cpu, Camino1player);
+        }
+        if (Camino1cpu.getCima().getValor().getTipo() == 2) {
+            interaccion.CombateArquero(Camino1cpu, Camino1player);
+        }
+        if (Camino1cpu.getCima().getValor().getTipo() == 3) {
+            interaccion.CombateCaballero(Camino1cpu, Camino1player);
+        }
+
+        //Validacion para interaccion entre Camino2 CPU y Player
+        if (Camino2cpu.getCima().getValor().getTipo() == 1) {
+            interaccion.CombateMago(Camino2cpu, Camino2player);
+        }
+        if (Camino2cpu.getCima().getValor().getTipo() == 2) {
+            interaccion.CombateArquero(Camino2cpu, Camino2player);
+        }
+        if (Camino2cpu.getCima().getValor().getTipo() == 3) {
+            interaccion.CombateCaballero(Camino2cpu, Camino2player);
+        }
 
     }
-    
+
 }

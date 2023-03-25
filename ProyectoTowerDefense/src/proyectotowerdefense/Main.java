8544fd67ba1abb18df508f97eb91f9ef9ccd9d75
JosethAG <93845320+JosethAG@users.noreply.github.com>
2023-03-24 22:49:23 -0600

    feat: Code Seleccion and main test code

--- ProyectoTowerDefense/src/proyectotowerdefense/Main.java
+++ ProyectoTowerDefense/src/proyectotowerdefense/Main.java
@@ -4,9 +4,12 @@
  */
 package proyectotowerdefense;
 
+import javax.swing.JOptionPane;
+
+
 /**
  *
- * @author Angelo
+ * @author Equipo4
  */
 public class Main {
 
@@ -20,7 +23,25 @@ public class Main {
         Pila Camino2cpu = new Pila();
         Pila Camino2player = new Pila();
         InteraccionesTropa interaccion = new InteraccionesTropa();
+        Castillo castilloCPU = new Castillo(1, 10, 1);
+        Castillo castilloPlayer = new Castillo(2, 10, 2);
+        int ronda = 1;
+        boolean repetir = true;
+        //Ciclo Principal
+        while(repetir){
+            //Menu
+            //Ciclo juego
+            while(castilloCPU.getVida() > 0 || castilloPlayer.getVida() > 0){
+                //preparacion
+                interaccion.Seleccion(Camino1player, Camino2player, ronda);
+                
 
+                //inicio
+                //interacciones
+                ronda++;
+            }
+        }
+        
         //Validacion para interaccion entre Camino1 CPU y Player
         if (Camino1cpu.getCima().getValor().getTipo() == 1) {
             interaccion.CombateMago(Camino1cpu, Camino1player);

6e90e8ed9d6af0eb4d50916a2b0bf1bced3f8813
Crizzi-11 <cmarin90677@ufide.ac.cr>
2023-04-17 11:14:26 -0600

    Feat: se hizo el primer punto
    Se logro hacer el primer punto solicitado!

--- ProyectoTowerDefense/src/vista/interaccionesTropas.java
+++ ProyectoTowerDefense/src/vista/interaccionesTropas.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package vista;
+
+import controlador.InteraccionesTropa;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Angelo
  */
 class interaccionesTropas {
+    
+    double vidaCastilloPlayer = 10;
+    double vidaCastilloCPU = 10;
+    frmJuego frmJuego;
+
+    public void verificaGanador(){
+        
+        if (vidaCastilloPlayer <= 0) {
+            JOptionPane.showMessageDialog(frmJuego, "GANADOR CPU", "WIN", 0);
+
+        } else if (vidaCastilloCPU <= 0) {
+            JOptionPane.showMessageDialog(frmJuego, "GANADOR PLAYER", "WIN", 0);
+
+        }
+    
+    }
+    
+    
     
 }

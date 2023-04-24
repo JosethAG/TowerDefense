8e5ced1589083baf30eb1a384b7ed62bd300a33a
JosethAG <93845320+JosethAG@users.noreply.github.com>
2023-04-24 10:35:45 -0600

    chore: Optimización  y limpieza do código

--- ProyectoTowerDefense/src/controlador/InteraccionesTropa.java
+++ ProyectoTowerDefense/src/controlador/InteraccionesTropa.java
@@ -1,73 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controlador;
 
 import vista.frmJuego;
-import vista.frmPreparacion;
 
 /**
- *
- * @author Equipo4
+ * @author Grupo 4
  */
 public class InteraccionesTropa {
 
-    double vidaCastilloPlayer = 10;
-    double vidaCastilloCPU = 10;
-    frmJuego frmJuego;
-    frmPreparacion frmPreparacion;
-    InteraccionUI InteraccionUI = new InteraccionUI();
+    frmJuego frmJuego; //Creamos referencia
 
     public void moverTropasCamino1() {
         //Lo que me permite es que cada vez que se llame en el hilo toma las posiciones de las tropas y las mueve
-
-        frmJuego.lblTropa1CPU.setLocation(frmJuego.lblTropa1CPU.getX() + 3, frmJuego.lblTropa1CPU.getY());
-        frmJuego.lblTropaPlayer1.setLocation(frmJuego.lblTropaPlayer1.getX() - 3, frmJuego.lblTropaPlayer1.getY());
-
+        frmJuego.lblTropa1CPU.setLocation(frmJuego.lblTropa1CPU.getX() + 3, frmJuego.lblTropa1CPU.getY()); //Movemos el lblCPU un "n" número de pixeles
+        frmJuego.lblTropaPlayer1.setLocation(frmJuego.lblTropaPlayer1.getX() - 3, frmJuego.lblTropaPlayer1.getY()); //Movemos el lblPLAYER un "n" número de pixeles
     }
-    
 
     public int verificaDueloTropas() {
         //Choque entre camino 1
-        if (frmJuego.lblTropaPlayer1.getX() <= (frmJuego.lblTropa1CPU.getX() + 61)) // && (lblTropaPlayer1.getX() + 188) > lblTropa1CPU.getX())
+        if (frmJuego.lblTropaPlayer1.getX() <= (frmJuego.lblTropa1CPU.getX() + 61)) // Verifica si la posición el lblCPU está cerca del lblPLAYER
         {
-            //     System.out.println("Duelo camino 1");
             return 1;
         }
-        
+
         return 0;
 
     }
 
     public int verificaDueloCastillo() { //Validacion para verificar si hubo algun enfrentamiento contra el castillo
         //Choque castillo Player en camino 1 
-        if (frmJuego.lblTropa1CPU.getX() >= frmJuego.lblCastilloPlayer.getX() - 40) // && (lblTropaPlayer1.getX() + 188) > lblTropa1CPU.getX())
+        if (frmJuego.lblTropa1CPU.getX() >= frmJuego.lblCastilloPlayer.getX() - 40) // Verifica si el lblCPU llegó al castillo del PLAYER
         {
-            //System.out.println("Choco Castillo Player Camino 1");
             return 1; //Retorna 1 si hubo un choque en el castillo del Player en el camino 1
-        } // Choque entre camino 1 castillo CPU
-        else if (frmJuego.lblTropaPlayer1.getX() <= (frmJuego.lblCastilloCPU.getX() + 307)) // && (lblTropaPlayer1.getX() + 188) > lblTropa1CPU.getX())
+        } else if (frmJuego.lblTropaPlayer1.getX() <= (frmJuego.lblCastilloCPU.getX() + 307)) // Verifica si el lblPLAYER llegó al castillo del CPU
         {
-            //   System.out.println("Choco Castillo CPU Camino 1");
             return 2; //Retorna 2 si hubo un choque en el castillo del CPU en el camino 1
         }
 
         return 0;
 
     }
-
-    public void verificaCastilloDestruido() {
-        if (vidaCastilloPlayer <= 0) {
-            InteraccionUI.ActualizaVidaCastillos(1, 0);
-            frmPreparacion.apagaHilo();
-
-        } else if (vidaCastilloCPU <= 0) {
-            InteraccionUI.ActualizaVidaCastillos(2, 0);
-            frmPreparacion.apagaHilo();
-
-        }
-
-    }
-
 }
